53b0df6928cd4a541069e6860d17b3a32a31463d
Hugo Lepage <hugo.lepage@gmail.com>
2025-08-02 00:17:51 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import vercel from "@astrojs/vercel";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hugolepage.github.io/games',
-  base: '/',
+  base: '/games/',
    output: 'static',
   vite: {
     plugins: [tailwindcss()]
