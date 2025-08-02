a0b45c647a674d763f2c7c6b91074e99b1fb9bfe
Hugo Lepage <hugo.lepage@gmail.com>
2025-08-02 01:27:05 +0100

    Remove node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,12 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://HugoLepage.github.io/games',
   base: '/games/',
   output: 'static', // Still use static output for GitHub Pages
-  adapter: node({
-    mode: 'standalone'
-  }),
   vite: {
     plugins: [tailwindcss()]
   },
